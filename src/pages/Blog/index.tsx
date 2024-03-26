baf5a624045ee15c332d6892a9382c156da92756
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-26 15:29:36 -0300

    feat: search input :sparkles:

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,9 +1,11 @@
 import { Profile } from "./components/Profile";
+import { SearchInput } from "./components/SearchInput";
 
 export const Blog = () => {
   return (
     <>
      <Profile />
+     <SearchInput />
     </>
   );
 }
