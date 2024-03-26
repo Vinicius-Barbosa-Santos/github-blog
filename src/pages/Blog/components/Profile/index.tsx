861ad3d1e7611178836927afcdd3a7e53be239f1
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-26 14:26:37 -0300

    feat: external link component :sparkles:

--- src/pages/Blog/components/Profile/index.tsx
+++ src/pages/Blog/components/Profile/index.tsx
@@ -1,9 +1,23 @@
-import { ProfileContainer, ProfilePicture } from "./styles"
+import { ExternalLink } from "../../../../components/ExternalLInk"
+import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles"
 
 export const Profile = () => {
-    return(
+    return (
         <ProfileContainer>
-            <ProfilePicture src={'https://avatars.githubusercontent.com/u/41848606?v=4'}  />
+            <ProfilePicture src={'https://avatars.githubusercontent.com/u/41848606?v=4'} />
+
+            <ProfileDetails>
+                <header>
+                    <h1>Vinícius</h1>
+
+                    <ExternalLink
+                        text="Github"
+                        href={'#'}
+                        target="_blank"
+                    />
+                </header>
+                <p>Dev</p>
+            </ProfileDetails>
         </ProfileContainer>
     )
 }
