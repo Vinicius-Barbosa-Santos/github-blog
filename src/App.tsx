e6738b3b9b527ccc992cc2fb0148a6be827204c9
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-26 12:30:08 -0300

    feat: global styles :gear:

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { BrowserRouter } from "react-router-dom"
-import { Router } from "./Router"
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Router } from "./Router";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./styles/theme/defaultTheme";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Router />
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+
+        <Router />
+      </ThemeProvider>
     </BrowserRouter>
-  )
+  );
 }
