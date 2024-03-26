7aa3b6ae2c2518b92dd994b48f55ecfe2c14058a
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-26 16:27:56 -0300

    feat: home page finish :sparkles:

---
+++ src/pages/Blog/components/Post/index.tsx
@@ -0,0 +1,16 @@
+import { PostContainer } from "./styles"
+
+export const Post = () => {
+    return (
+        <PostContainer>
+            <div>
+                <strong>JavaScript data types and data structures</strong>
+                <span>Há 1 dia</span>
+            </div>
+
+            <p>
+                Programming languages all have built-in data structures, but these often differ from one language to another. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptatem totam ratione libero ab labore molestias alias in? Minima nemo quaerat veritatis ipsa modi hic error accusamus quo aspernatur vero!
+            </p>
+        </PostContainer>
+    )
+}
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,24 @@
+import { Post } from "./components/Post";
 import { Profile } from "./components/Profile";
 import { SearchInput } from "./components/SearchInput";
+import { PostsListContainer } from "./styles";
 
 export const Blog = () => {
   return (
     <>
      <Profile />
      <SearchInput />
+     <PostsListContainer>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+     </PostsListContainer>
     </>
   );
 }
