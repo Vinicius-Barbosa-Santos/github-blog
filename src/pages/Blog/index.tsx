c90790e012f4275bdf098eb2be044b5261a3c138
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-27 00:38:28 -0300

    feat: connecting github API :sparkles:

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,14 +1,43 @@
+import { useCallback, useEffect, useState } from "react";
 import { Post } from "./components/Post";
 import { Profile } from "./components/Profile";
 import { SearchInput } from "./components/SearchInput";
 import { PostsListContainer } from "./styles";
+import { api } from "../../lib/axios";
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const repoName = import.meta.env.VITE_GITHUB_REPONAME
+
+interface IPost {
+  title: string
+}
 
 export const Blog = () => {
+
+  const [posts, setPosts] = useState<IPost[]>([])
+
+  const getPosts = useCallback(async (query: string = "") => {
+    try {
+      const response = await api.get(`/search/issues?q=${query}%20repo:${username}/${repoName}`)
+
+      setPosts(response.data.items)
+
+      // eslint-disable-next-line no-empty
+    } finally {
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [posts]
+  )
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <>
-     <Profile />
-     <SearchInput />
-     <PostsListContainer>
+      <Profile />
+      <SearchInput />
+      <PostsListContainer>
         <Post />
         <Post />
         <Post />
@@ -18,7 +47,7 @@ export const Blog = () => {
         <Post />
         <Post />
         <Post />
-     </PostsListContainer>
+      </PostsListContainer>
     </>
   );
 }
