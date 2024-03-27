e92a77d02a7218504d98f7b5756c2f1f33c92e7a
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-27 15:48:09 -0300

    feat: get post by id :sparkles:

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,39 @@
+import { useCallback, useEffect, useState } from "react"
 import { PostHeader } from "./components/PostHeader"
+import { IPost } from "../Blog"
+import { api } from "../../lib/axios"
+import { useParams } from "react-router-dom"
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const repoName = import.meta.env.VITE_GITHUB_REPONAME
 
 export const Post = () => {
-    return(
+    const [postData, setPostData] = useState<IPost>({} as IPost)
+    const [isLoading, setIsLoading] = useState(true)
+
+    const { id } = useParams()
+
+    const getPostDetails = useCallback(async () => {
+        try {
+            setIsLoading(true)
+            const response = await api.get(`/repos/${username}/${repoName}/issues/${id}`)
+
+            setPostData(response.data)
+
+        } finally {
+            setIsLoading(false)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [postData])
+
+    useEffect(() => {
+        getPostDetails()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
         <>
-            <PostHeader />
+            <PostHeader isLoading={isLoading} postData={postData} />
         </>
     )
 }
