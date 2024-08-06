0a5c76c33aa58d1b416752d6695529244e32fc00
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-08-06 16:18:13 -0300

    fix: inline deleted :gear:

--- src/pages/Post/components/PostContent/index.tsx
+++ src/pages/Post/components/PostContent/index.tsx
@@ -14,9 +14,9 @@ export function PostContent({ content }: PostContentProps) {
         children={content}
         components={{
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          code({ node, inline, className, children, ...props }) {
+          code({ node, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || "");
-            return !inline && match ? (
+            return match ? (
               <SyntaxHighlighter
                 children={String(children).replace(/\n$/, "")}
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
